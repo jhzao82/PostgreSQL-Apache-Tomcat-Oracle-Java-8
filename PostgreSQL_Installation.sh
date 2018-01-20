572cd1f91af765f9b8841e36f48ff299bd0ca59d
Furkan Buyrukoglu <furkanbuyrukoglu@gmail.com>
2018-01-20 21:46:16 +0300

    PostgreSQL installation added

---
+++ PostgreSQL_Installation.sh
@@ -0,0 +1,10 @@
+# Install the repository RPM
+yum install https://download.postgresql.org/pub/repos/yum/10/redhat/rhel-7-x86_64/pgdg-centos10-10-1.noarch.rpm
+
+# Install client, server and contributed binaries packages
+yum install postgresql10 postgresql10-server postgresql10-contrib
+
+# Initialize the database and enable automatic start
+/usr/pgsql-10/bin/postgresql-10-setup initdb
+systemctl enable postgresql-10
+systemctl start postgresql-10
