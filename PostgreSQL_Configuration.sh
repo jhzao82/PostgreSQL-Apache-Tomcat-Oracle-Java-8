f71a2a895d42bc298d209bc03a357482e7207116
Furkan Buyrukoglu <furkanbuyrukoglu@gmail.com>
2018-01-20 22:44:39 +0300

    postgresql configuration file updated

---
+++ PostgreSQL_Configuration.sh
@@ -0,0 +1,14 @@
+# vi /var/lib/pgsql/10/data/pg_hba.conf
+
+echo 'local all all trust' >> /var/lib/pgsql/10/data/pg_hba.conf
+echo 'local atar atar trust' >> /var/lib/pgsql/10/data/pg_hba.conf
+echo 'host all all 127.0.0.1/32 trust' >> /var/lib/pgsql/10/data/pg_hba.conf
+echo 'host all all ::1/128 ident' >> /var/lib/pgsql/10/data/pg_hba.conf
+
+systemctl restart postgresql-10.service
+su - postgres
+createdb atar;
+psql
+CREATE ROLE "atar" WITH PASSWORD 'atar1q2w3e4r';
+GRANT ALL PRIVILEGES ON DATABASE atar TO atar;
+ALTER ROLE atar WITH LOGIN;
