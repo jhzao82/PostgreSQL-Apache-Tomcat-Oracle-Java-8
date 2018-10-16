fe906f2ae892795b40b8ef523d9cd34bc578f99a
jhzao82 <jhzao82@126.com>
2018-10-16 13:03:11 +0800

    Update OracleJava8InstallConfigure.sh

--- OracleJava8InstallConfigure.sh
+++ OracleJava8InstallConfigure.sh
@@ -5,12 +5,12 @@ cd /opt
 wget --no-cookies \
 --no-check-certificate \
 --header "Cookie: oraclelicense=accept-securebackup-cookie" \
-http://download.oracle.com/otn-pub/java/jdk/8u171-b11/512cd62ec5174c3487ac17c61aaa89e8/jdk-8u171-linux-x64.tar.gz
-tar xzf jdk-8u171-linux-x64.tar.gz
+http://download.oracle.com/otn-pub/java/jdk/8u181-b13/96a7b8442fe848ef90c96a2fad6ed6d1/jdk-8u181-linux-x64.tar.gz
+tar xzf jdk-8u181-linux-x64.tar.gz
 
 # Install Java 8 with Alternatives
-cd /opt/jdk1.8.0_171/
-alternatives --install /usr/bin/java java /opt/jdk1.8.0_171/bin/java 2
+cd /opt/jdk1.8.0_181/
+alternatives --install /usr/bin/java java /opt/jdk1.8.0_181/bin/java 2
 
 # There is 1 program that provides 'java'.
 #
@@ -20,14 +20,14 @@ alternatives --install /usr/bin/java java /opt/jdk1.8.0_171/bin/java 2
 
 # At this point JAVA 8 has been successfully installed on your system. We also recommend to setup javac and jar commands path using alternatives
 
-alternatives --install /usr/bin/jar jar /opt/jdk1.8.0_171/bin/jar 2
-alternatives --install /usr/bin/javac javac /opt/jdk1.8.0_171/bin/javac 2
-alternatives --set jar /opt/jdk1.8.0_171/bin/jar
+alternatives --install /usr/bin/jar jar /opt/jdk1.8.0_181/bin/jar 2
+alternatives --install /usr/bin/javac javac /opt/jdk1.8.0_181/bin/javac 2
+alternatives --set jar /opt/jdk1.8.0_181/bin/jar
 
 # Check installed Java version
 java -version
 
 # Setup Java Environment Variables
-export JAVA_HOME=/opt/jdk1.8.0_171
-export JRE_HOME=/opt/jdk1.8.0_171/jre
-export PATH=$PATH:/opt/jdk1.8.0_171/bin:/opt/jdk1.8.0_171/jre/bin
+export JAVA_HOME=/opt/jdk1.8.0_181
+export JRE_HOME=/opt/jdk1.8.0_181/jre
+export PATH=$PATH:/opt/jdk1.8.0_181/bin:/opt/jdk1.8.0_181/jre/bin
